67ddded515039f4f6cfe0fc6e013f0714d432311
izzalDev <rizal.fadlullah@gmail.com>
2025-09-01 22:47:29 +0700

    Added package for support Snacks Nvim picker in windows installation script

--- install.ps1
+++ install.ps1
@@ -8,8 +8,9 @@ if ( -not (Get-command scoop -ErrorAction SilentlyContinue)){
 
 scoop bucket add versions
 scoop bucket add nerd-fonts
-scoop install JetBrainsMono-NF
-scoop install wezterm-nightly
+scoop bucket add extras
+scoop install nerd-fonts/JetBrainsMono-NF
+scoop install versions/wezterm-nightly
 scoop install neovim
 scoop install msys2
 scoop install which
@@ -17,9 +18,12 @@ scoop install gh
 scoop install gcc
 scoop install nodejs
 scoop install starship
+scoop install extras/lazygit
+scoop install grep
+scoop install fd
 
 git clone https://github.com/izzaldev/nvim $env:LOCALAPPDATA\nvim
 
-New-Item -Path (&Split-Path $PROFILE.CurrentUserAllHosts) -ItemType Directory
-New-Item -Path $PROFILE.CurrentUserAllHosts -ItemType File
+New-Item -Path (&Split-Path $PROFILE.CurrentUserAllHosts) -ItemType Directory -Force
+New-Item -Path $PROFILE.CurrentUserAllHosts -ItemType File -Force
 Write-Output "starship init powershell | Invoke-Expression" > $PROFILE.CurrentUserAllHosts
