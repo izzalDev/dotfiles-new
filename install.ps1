29c8d209454d13257895ad88f4921e1f902bb743
izzalDev <rizal.fadlullah@gmail.com>
2025-09-03 11:47:56 +0700

    feat: Refactor installation script to run admin commands and update profile setup

--- install.ps1
+++ install.ps1
@@ -1,18 +1,28 @@
-winget install --id Microsoft.VCRedist.2015+.x86
-winget install --id Microsoft.VCRedist.2015+.x64
-winget install --id Microsoft.PowerShell
+$IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+$adminCommands = @'
+winget install --id Microsoft.VCRedist.2015+.x64 --accept-source-agreements --accept-package-agreements
+winget install --id Microsoft.PowerShell --accept-source-agreements --accept-package-agreements
+winget install --id Git.Git --accept-source-agreements --accept-package-agreements
+'@
 
-if ( -not (Get-command scoop -ErrorAction SilentlyContinue)){
- Invoke-RestMethod get.scoop.sh | Invoke-Expression
+if (-not $IsAdmin) {
+    $proc = Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile", "-Command", $adminCommands -PassThru
+    $proc.WaitForExit()
 }
 
+if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+    Invoke-RestMethod get.scoop.sh | Invoke-Expression
+}
+
+scoop install aria2 
+scoop config aria2-warning-enabled false
 scoop bucket add versions
 scoop bucket add nerd-fonts
 scoop bucket add extras
+
 scoop install nerd-fonts/JetBrainsMono-NF
 scoop install versions/wezterm-nightly
 scoop install neovim
-scoop install msys2
 scoop install which
 scoop install gh
 scoop install gcc
@@ -21,9 +31,38 @@ scoop install starship
 scoop install extras/lazygit
 scoop install grep
 scoop install fd
+scoop install tre-command
+scoop install busybox
+scoop install komorebi
+scoop install yazi
 
 git clone https://github.com/izzaldev/nvim $env:LOCALAPPDATA\nvim
 
-New-Item -Path (&Split-Path $PROFILE.CurrentUserAllHosts) -ItemType Directory -Force
-New-Item -Path $PROFILE.CurrentUserAllHosts -ItemType File -Force
-Write-Output "starship init powershell | Invoke-Expression" > $PROFILE.CurrentUserAllHosts
+$profileScript = @'
+Get-Alias |
+  Where-Object { "$($_.Options) $($_.Name)" -notmatch 'ReadOnly|Constant|cd' } |
+  ForEach-Object { Remove-Item "Alias:$($_.Name)" }
+
+function y {
+  $tmp = (New-TemporaryFile).FullName
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp -Encoding UTF8
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+      Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+  }
+  Remove-Item -Path $tmp
+}
+
+function clear {
+  "`n" * $Host.UI.RawUI.WindowSize.Height | Out-Host
+  [Console]::SetCursorPosition(0, 0)
+}
+
+starship init powershell | Invoke-Expression
+'@
+
+[System.Environment]::SetEnvironmentVariable("YAZI_FILE_ONE", "C:\Program Files\Git\usr\bin\file.EXE", 'User')
+
+New-Item -Path "$env:USERPROFILE\Documents\PowerShell" -ItemType Directory -Force
+New-Item -Path "$env:USERPROFILE\Documents\PowerShell\profile.ps1" -ItemType File -Force
+$profileScript > $env:USERPROFILE\Documents\PowerShell\profile.ps1
